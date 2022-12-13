85f7a70fa980090dc8a365786c63abe7ffe031a8
Gakles <87351405+Gakles@users.noreply.github.com>
2022-12-13 09:38:57 -0800

    started player vehicle object thing

---
+++ PCV.java
@@ -0,0 +1,18 @@
+public class PCV {
+
+    public PCV(int Tank){
+        if (Tank == 0){
+            //LVKV 9040
+            //Basic Movment
+            //in km/h
+            int topSpeed = 70;
+            //in m/s - not sure if this is a realist acceleration
+            int vehicleAcceleration = 10;
+            //in m
+            int length = 7;
+            //in m
+            int width = 3.5;
+            
+        }
+    }
+}
